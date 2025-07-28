aee4478a1bddf22d22339c26ec31d415d135207f
Andreas <Andreas.Zwander@gmail.com>
2025-07-28 21:46:21 +0200

    Add IniComment class implementation in IniTools namespace

---
+++ IniTools/Classes/IniComment.cs
@@ -0,0 +1,8 @@
+﻿using IniTools.Interfaces;
+
+namespace IniTools.Classes;
+
+public sealed class IniComment(string? comment) : IIniComment
+{
+    public string? Comment { get; set; } = comment;
+}
