ac5aaffad3c712b9c44f919d4e588fab14aaf818
Andreas <Andreas.Zwander@gmail.com>
2025-07-28 23:41:43 +0200

    Add IniParserSettings class definition in Scratch folder

---
+++ IniTools/Scratch/IniParserSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace IniTools.Scratch;
+
+public sealed class IniParserSettings : IIniSettings
+{
+    public readonly List<string> CommentPrefixes = [];
+    public readonly List<string> KeyValueSeparators = [];
+    public static IniParserSettings Create => new IniParserSettings();
+    public static IniParserSettings Default => Create.WithDefaultValues();
+}
