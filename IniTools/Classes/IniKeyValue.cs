2024369311c9fbc7c6e769739627444a57333d7d
Andreas <Andreas.Zwander@gmail.com>
2025-07-28 21:46:33 +0200

    Add IniKeyValue class implementation in IniTools namespace

---
+++ IniTools/Classes/IniKeyValue.cs
@@ -0,0 +1,9 @@
+﻿using IniTools.Interfaces;
+
+namespace IniTools.Classes;
+
+public sealed class IniKeyValue(string? key, string? value) : IIniKeyValue
+{
+    public string? Key { get; set; } = key;
+    public string? Value { get; set; } = value;
+}
