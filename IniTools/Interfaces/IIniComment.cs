d760d4eb44761e8a879289554f1f9444c008a7d1
Andreas <Andreas.Zwander@gmail.com>
2025-07-28 21:08:48 +0200

    Add IIniComment interface in IniTools namespace

---
+++ IniTools/Interfaces/IIniComment.cs
@@ -0,0 +1,8 @@
+﻿namespace IniTools.Interfaces;
+
+public interface IIniComment : IIniLine, IIniListAble
+{
+    string Prefix { get; set; }
+    string Separator { get; set; }
+    string Comment { get; set; }
+}
