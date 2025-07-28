716019f218e63a9aab2b66be31e90150538ed4db
Andreas <Andreas.Zwander@gmail.com>
2025-07-28 22:04:55 +0200

    Add IniSection class implementation in IniTools namespace

---
+++ IniTools/Classes/IniSection.cs
@@ -0,0 +1,9 @@
+﻿using IniTools.Interfaces;
+
+namespace IniTools.Classes;
+
+public sealed class IniSection(IIniSectionName? name) : IIniSection
+{
+    public IIniSectionName? Name { get; } = name;
+    public List<IIniListAble> Elements { get; } = [];
+}
