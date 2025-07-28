a6a4522812b76ebb0a44e54f5e278b6dee5dea61
Andreas <Andreas.Zwander@gmail.com>
2025-07-28 22:36:01 +0200

    Add IniParserSettings class implementation in IniTools namespace

---
+++ IniTools/Scratch/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿namespace IniTools.Scratch;
+
+public interface IIniSettings;
+
+public sealed class IniParserSettings : IIniSettings
+{
+    public readonly List<string> CommentPrefixes = [];
+    public readonly List<string> KeyValueSeparators = [];
+    public IniParserSettings()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        CommentPrefixes.Add(";");
+        CommentPrefixes.Add("#");
+        KeyValueSeparators.Add("=");
+        KeyValueSeparators.Add(":");
+    }
+}
