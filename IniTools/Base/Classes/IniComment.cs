9c89e047f255de218e35e63a9eac385c9fbe1ff4
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:12:09 +0200

    Add `Equals` and `CompareTo` method stubs to `IniComment`.

--- IniTools/Base/Classes/IniComment.cs
+++ IniTools/Base/Classes/IniComment.cs
@@ -5,4 +5,7 @@ namespace IniTools.Base.Classes;
 public sealed class IniComment(string? comment) : IIniComment
 {
     public string? Comment { get; set; } = comment;
+    public bool Equals ( IIniComment other ) => throw new System.NotImplementedException();
+
+    public int CompareTo ( IIniComment other ) => throw new System.NotImplementedException();
 }
