d42e8fea71ffa324bfc85977e1819e638f25d3b8
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:27:13 +0200

    Implement `Equals`, `CompareTo`, and related operators in `IniEmptyLine`.

--- IniTools/Base/Classes/IniEmptyLine.cs
+++ IniTools/Base/Classes/IniEmptyLine.cs
@@ -4,7 +4,10 @@ namespace IniTools.Base.Classes;
 
 public sealed class IniEmptyLine : IIniEmptyLine
 {
-    public bool Equals ( IIniEmptyLine other ) => throw new System.NotImplementedException();
-
-    public int CompareTo ( IIniEmptyLine other ) => throw new System.NotImplementedException();
+    public int CompareTo ( IIniEmptyLine? other ) { return other is null ? 1 : 0; }
+    public bool Equals ( IIniEmptyLine? other ) { return other is not null; }
+    public override bool Equals ( object? obj ) { return Equals ( obj as IIniEmptyLine ); }
+    public override int GetHashCode() { return 0; }
+    public static bool operator == ( IniEmptyLine? left , IniEmptyLine? right ) { return left is null && right is null || left is not null && left.Equals ( right ); }
+    public static bool operator != ( IniEmptyLine? left , IniEmptyLine? right ) => !( left == right );
 }
