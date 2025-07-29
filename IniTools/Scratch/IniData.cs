0e65ecfdbeb6acdc4fa826979bbb5ebce84bb8e9
Andreas <Andreas.Zwander@gmail.com>
2025-07-30 01:05:17 +0200

    Add utility methods and fluent API to `IniData` for improved section and key-value handling.

--- IniTools/Scratch/IniData.cs
+++ IniTools/Scratch/IniData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using IniTools.Base.Classes;
 using IniTools.Base.Interfaces;
 
 namespace IniTools.Scratch;
@@ -35,5 +37,39 @@ public class IniData : IDictionary< string , IIniSection >
     public bool Remove ( KeyValuePair< string , IIniSection > item ) => ( (ICollection< KeyValuePair< string , IIniSection > >) _sections ).Remove ( item );
     public IEnumerator< KeyValuePair< string , IIniSection > > GetEnumerator() => _sections.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    #endregion
+
+    #region Verbesserte Hilfsmethoden
+    public string? GetValue ( string sectionName , string key ) => TryGetValue ( sectionName , out var section ) ? section.Elements.OfType< IIniKeyValue >().FirstOrDefault ( kv => string.Equals ( kv.Key , key , StringComparison.OrdinalIgnoreCase ) )?.Value : null;
+
+    public void SetValue ( string sectionName , string key , string value )
+    {
+        if ( !TryGetValue ( sectionName , out var section ) ) {
+            section = new IniSection ( sectionName );
+            Add ( section );
+        }
+
+        var keyValuePair = section.Elements.OfType< IIniKeyValue >().FirstOrDefault ( kv => string.Equals ( kv.Key , key , StringComparison.OrdinalIgnoreCase ) );
+
+        if ( keyValuePair != null ) { keyValuePair.Value = value; }
+        else { section.Elements.Add ( new IniKeyValue ( key , value ) ); }
+    }
+    #endregion
+
+    #region Fluent API
+    /// <summary>
+    /// Startet die Fluent API, um eine Sektion zu erstellen oder zu bearbeiten.
+    /// </summary>
+    /// <param name="sectionName">Der Name der Sektion.</param>
+    /// <returns>Einen IniSectionBuilder, um Methoden zu verketten.</returns>
+    public IniSectionBuilder WithSection ( string sectionName )
+    {
+        if ( TryGetValue ( sectionName , out var section ) ) { return new IniSectionBuilder ( this , section ); }
+
+        section = new IniSection ( sectionName );
+        Add ( section );
+
+        return new IniSectionBuilder ( this , section );
+    }
     #endregion
 }
