0322bd4c8123f0fa82729864d1e84da676abad96
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:11:41 +0200

    Update `IIniComment` interface to inherit from `IIniLine<IIniComment>` instead of `IIniLine`.

--- IniTools/Base/Interfaces/IIniComment.cs
+++ IniTools/Base/Interfaces/IIniComment.cs
@@ -1,6 +1,6 @@
 ﻿namespace IniTools.Base.Interfaces;
 
-public interface IIniComment : IIniLine, IIniSectionAddAble
+public interface IIniComment : IIniLine<IIniComment>, IIniSectionAddAble
 {
     string? Comment { get; set; }
 }
