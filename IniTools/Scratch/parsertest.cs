2cca53e5791c3854ae93f783e56c7b11c2b5ff8b
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 04:48:14 +0200

    Add method to read and process lines from file in parsertest.cs

--- IniTools/Scratch/parsertest.cs
+++ IniTools/Scratch/parsertest.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IniTools.Scratch;
+
+public class Scratch
+{
+    private static readonly string[] LineBreaks = ["\r\n", "\r", "\n"];
+    public static List<string> GetLines(string filePath) => ProcessTextIntoLines(File.ReadAllText(filePath));
+    private static List<string> ProcessTextIntoLines(string text) => text.Split(LineBreaks, StringSplitOptions.None).Select(line => line.Trim()).ToList();
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+       List<string> lines = GetLines(@"C:\Users\Andreas\AppData\Roaming\Mozilla\Firefox\profiles.ini");
+       foreach (var line in lines)
+           Console.WriteLine(line);
+    }
+}
