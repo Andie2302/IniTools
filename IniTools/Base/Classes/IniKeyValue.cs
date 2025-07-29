5a43731b866b59d8b2d46b36adfd9d6c239f1dfe
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:14:38 +0200

    Add `Equals` and `CompareTo` method stubs to `IniKeyValue`.

--- IniTools/Base/Classes/IniKeyValue.cs
+++ IniTools/Base/Classes/IniKeyValue.cs
@@ -6,4 +6,7 @@ public sealed class IniKeyValue(string? key, string? value) : IIniKeyValue
 {
     public string? Key { get; set; } = key;
     public string? Value { get; set; } = value;
+    public bool Equals ( IIniKeyValue other ) => throw new System.NotImplementedException();
+
+    public int CompareTo ( IIniKeyValue other ) => throw new System.NotImplementedException();
 }
