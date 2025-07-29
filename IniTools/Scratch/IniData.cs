44fdae0b51cf8e86106ac2a51f2f5fd84e275a98
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 22:25:30 +0200

    Refactor `IniData` class to implement `IDictionary<IIniSectionName, IIniSection>` and add string-based utility methods.

--- IniTools/Scratch/IniData.cs
+++ IniTools/Scratch/IniData.cs
@@ -1,38 +1,67 @@
 ﻿using System.Collections.Generic;
 using IniTools.Base.Interfaces;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using IniTools.Base.Classes;
+
+namespace IniTools.Scratch;
 
 public class IniData : IDictionary< IIniSectionName , IIniSection >
 {
-    private readonly Dictionary< IIniSectionName , IIniSection > _sections = new Dictionary< IIniSectionName , IIniSection >();
+    private readonly Dictionary< IIniSectionName , IIniSection > _sections = new( new IniSectionNameComparer() );
 
+    #region String-basierte Hilfsmethoden
+    /// <summary>
+    /// Ruft eine Sektion über ihren Namen ab oder legt sie fest.
+    /// </summary>
+    public IIniSection this [ string key ]
+    {
+        get => _sections[new IniSectionName ( key )];
+        set => _sections[new IniSectionName ( key )] = value;
+    }
+
+    /// <summary>
+    /// Prüft, ob eine Sektion mit dem gegebenen Namen existiert.
+    /// </summary>
+    public bool ContainsKey ( string key ) => _sections.ContainsKey ( new IniSectionName ( key ) );
+
+    /// <summary>
+    /// Versucht, eine Sektion sicher abzurufen.
+    /// </summary>
+    public bool TryGetValue ( string key , [ MaybeNullWhen ( false ) ] out IIniSection value ) { return _sections.TryGetValue ( new IniSectionName ( key ) , out value ); }
+
+    /// <summary>
+    /// Entfernt eine Sektion anhand ihres Namens.
+    /// </summary>
+    public bool Remove ( string key ) => _sections.Remove ( new IniSectionName ( key ) );
+    #endregion
+
+    #region IDictionary<IIniSectionName, IIniSection> Implementierung
+    public IIniSection this [ IIniSectionName key ]
+    {
+        get => _sections[key];
+        set => _sections[key] = value;
+    }
+    public ICollection< IIniSectionName > Keys => _sections.Keys;
+    public ICollection< IIniSection > Values => _sections.Values;
+    public int Count => _sections.Count;
+    public bool IsReadOnly => ( (ICollection< KeyValuePair< IIniSectionName , IIniSection > >) _sections ).IsReadOnly;
+    public void Add ( IIniSectionName key , IIniSection value ) => _sections.Add ( key , value );
+
+    public void Add ( IIniSection section )
+    {
+        if ( section.Name != null ) { _sections.Add ( section.Name , section ); }
+    }
+
+    public void Add ( KeyValuePair< IIniSectionName , IIniSection > item ) => _sections.Add ( item.Key , item.Value );
+    public bool ContainsKey ( IIniSectionName key ) => _sections.ContainsKey ( key );
+    public bool TryGetValue ( IIniSectionName key , [ MaybeNullWhen ( false ) ] out IIniSection value ) => _sections.TryGetValue ( key , out value );
+    public bool Remove ( IIniSectionName key ) => _sections.Remove ( key );
+    public bool Remove ( KeyValuePair< IIniSectionName , IIniSection > item ) => ( (ICollection< KeyValuePair< IIniSectionName , IIniSection > >) _sections ).Remove ( item );
+    public bool Contains ( KeyValuePair< IIniSectionName , IIniSection > item ) => ( (ICollection< KeyValuePair< IIniSectionName , IIniSection > >) _sections ).Contains ( item );
+    public void Clear() => _sections.Clear();
+    public void CopyTo ( KeyValuePair< IIniSectionName , IIniSection >[] array , int arrayIndex ) => ( (ICollection< KeyValuePair< IIniSectionName , IIniSection > >) _sections ).CopyTo ( array , arrayIndex );
+    public IEnumerator< KeyValuePair< IIniSectionName , IIniSection > > GetEnumerator() => _sections.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => _sections.GetEnumerator();
+    #endregion
 }
-
-/*
-   private readonly Dictionary< IIniSectionName , IIniSection > _sections = new Dictionary< IIniSectionName , IIniSection >();
-
-   public IIniSection this [ string key ]
-   {
-       get => ContainsKey ( key ) ? _sections[ToSectionKey ( key )] : IniSection.Create ( ToSectionKey ( key ) );
-       set => _sections[ToSectionKey ( key )] = value;
-   }
-   public void Add ( IIniSection section ) { Add ( ToSectionKey ( section.Name?.Value ) , section ); }
-   public void Add ( string key , IIniSection value ) { _sections.Add ( ToSectionKey ( key ) , value ); }
-   public bool Remove ( IIniSection section ) { return _sections.Remove ( ToSectionKey ( section.Name?.Value ) ); }
-
-   #region IDictionary Implementation
-   public ICollection< string > Keys => _sections.Keys;
-   public ICollection< IIniSection > Values => _sections.Values;
-   public int Count => _sections.Count;
-   public bool IsReadOnly => false;
-   public bool ContainsKey ( string key ) => _sections.ContainsKey ( ToSectionKey ( key ) );
-   public bool TryGetValue ( string key , out IIniSection value ) { return ( value = _sections[ToSectionKey ( key )] ) != null; }
-   public bool Remove ( string key ) => _sections.Remove ( ToSectionKey ( key ) );
-   public void Clear() => _sections.Clear();
-   public IEnumerator< KeyValuePair< string , IIniSection > > GetEnumerator() => _sections.GetEnumerator();
-   IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-   public void Add ( KeyValuePair< string , IIniSection > item ) => _sections.Add ( item.Key , item.Value );
-   public bool Contains ( KeyValuePair< string , IIniSection > item ) => _sections.ContainsKey ( item.Key );
-   public void CopyTo ( KeyValuePair< string , IIniSection >[] array , int arrayIndex ) => ( (ICollection< KeyValuePair< string , IIniSection > >) _sections ).CopyTo ( array , arrayIndex );
-   public bool Remove ( KeyValuePair< string , IIniSection > item ) => _sections.Remove ( item.Key );
-   #endregion
-   //*/
