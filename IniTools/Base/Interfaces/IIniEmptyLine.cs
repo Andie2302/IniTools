f765526e81e4d6137af83d599dca5cf93cbde8f1
Andreas <Andreas.Zwander@gmail.com>
2025-07-30 01:37:36 +0200

    Add XML documentation to `IIniEmptyLine` interface for better clarity

--- IniTools/Base/Interfaces/IIniEmptyLine.cs
+++ IniTools/Base/Interfaces/IIniEmptyLine.cs
@@ -1,3 +1,12 @@
 ﻿namespace IniTools.Base.Interfaces;
 
+/// <summary>
+/// Represents an empty line within an INI configuration.
+/// This interface is used to define and manage the behavior of empty lines
+/// that appear in INI files, typically for formatting or organizational purposes.
+/// </summary>
+/// <remarks>
+/// IIniEmptyLine extends the functionality from IIniLine and IIniSectionAddAble
+/// to provide behavior specific to empty lines in the context of INI structures.
+/// </remarks>
 public interface IIniEmptyLine : IIniLine<IIniEmptyLine>, IIniSectionAddAble;
