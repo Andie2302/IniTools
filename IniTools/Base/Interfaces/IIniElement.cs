f237e8aed2e112e7dd820bd8e050a85fd70d39d7
Andreas <Andreas.Zwander@gmail.com>
2025-07-30 01:37:31 +0200

    Add XML documentation to `IIniElement` interface for better clarity

--- IniTools/Base/Interfaces/IIniElement.cs
+++ IniTools/Base/Interfaces/IIniElement.cs
@@ -1,3 +1,8 @@
 ﻿namespace IniTools.Base.Interfaces;
 
+/// <summary>
+/// Represents a base element of an INI structure. This interface serves as the
+/// root abstraction for elements contained within an INI configuration,
+/// enabling common behavior or characteristics across specific INI components.
+/// </summary>
 public interface IIniElement;
