2b2b0cc4d3dbeecfdab38eea597a011c99b9095c
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:14:46 +0200

    Add `Equals` and `CompareTo` method stubs to `IniUnknownLine`.

--- IniTools/Base/Classes/IniUnknownLine.cs
+++ IniTools/Base/Classes/IniUnknownLine.cs
@@ -5,4 +5,7 @@ namespace IniTools.Base.Classes;
 public sealed class IniUnknownLine(string? line) : IIniUnknownLine
 {
     public string? Line { get; set; } = line;
+    public bool Equals ( IIniUnknownLine other ) => throw new System.NotImplementedException();
+
+    public int CompareTo ( IIniUnknownLine other ) => throw new System.NotImplementedException();
 }
