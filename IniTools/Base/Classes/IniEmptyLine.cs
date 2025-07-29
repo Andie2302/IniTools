79de0a9b86a473e7aec56737e5f5326aa4acc261
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:14:33 +0200

    Add `Equals` and `CompareTo` method stubs to `IniEmptyLine`.

--- IniTools/Base/Classes/IniEmptyLine.cs
+++ IniTools/Base/Classes/IniEmptyLine.cs
@@ -2,4 +2,9 @@
 
 namespace IniTools.Base.Classes;
 
-public sealed class IniEmptyLine : IIniEmptyLine;
+public sealed class IniEmptyLine : IIniEmptyLine
+{
+    public bool Equals ( IIniEmptyLine other ) => throw new System.NotImplementedException();
+
+    public int CompareTo ( IIniEmptyLine other ) => throw new System.NotImplementedException();
+}
