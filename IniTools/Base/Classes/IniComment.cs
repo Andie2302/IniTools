c0d1d17fc51b568b72cca658c07c29a9d31bb35a
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:27:08 +0200

    Implement `Equals`, `CompareTo`, and related operators in `IniComment`.

--- IniTools/Base/Classes/IniComment.cs
+++ IniTools/Base/Classes/IniComment.cs
@@ -1,11 +1,53 @@
-﻿using IniTools.Base.Interfaces;
+﻿using System;
+using IniTools.Base.Interfaces;
 
 namespace IniTools.Base.Classes;
 
 public sealed class IniComment(string? comment) : IIniComment
 {
     public string? Comment { get; set; } = comment;
-    public bool Equals ( IIniComment other ) => throw new System.NotImplementedException();
 
-    public int CompareTo ( IIniComment other ) => throw new System.NotImplementedException();
+    public int CompareTo(IIniComment? other) 
+    {
+        if (other is null) {
+            return 1;
+        }
+
+        return string.Compare(Comment, other.Comment, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public bool Equals(IIniComment? other)
+    {
+        if (other is null) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other)) {
+            return true;
+        }
+
+        return string.Equals(Comment, other.Comment, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as IIniComment);
+    }
+
+    public override int GetHashCode()
+    {
+        // ToLowerInvariant() für .NET Standard 2.0 Kompatibilität
+        return (Comment?.ToLowerInvariant() ?? "").GetHashCode();
+    }
+    
+    public static bool operator ==(IniComment? left, IniComment? right)
+    {
+        if (left is null) {
+            return right is null;
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(IniComment? left, IniComment? right) => !(left == right);
 }
