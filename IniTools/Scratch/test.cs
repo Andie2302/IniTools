3bb37471ecafcb5c100655c6b9908bf75cbb18ab
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 21:45:52 +0200

    Add `test.cs` file to demonstrate `IniData.TryGetValue` usage in `IniTools.Scratch`.

---
+++ IniTools/Scratch/test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IniTools.Scratch;
+
+public class test
+{
+    public static void Main()
+    {
+        IniData iniData = new();
+        var b = iniData.TryGetValue ( "peter" , out var a);
+        Console.Out.WriteLine(b);
+    }
+}
