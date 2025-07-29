ec4aa3b2c63fbbf06c41657eeeb1200dbfa63335
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 12:46:36 +0200

    Move IniEmptyLine class to Base.Classes namespace and update its interface namespace

---
+++ IniTools/Base/Classes/IniEmptyLine.cs
@@ -0,0 +1,5 @@
+﻿using IniTools.Base.Interfaces;
+
+namespace IniTools.Base.Classes;
+
+public sealed class IniEmptyLine : IIniEmptyLine;
--- IniTools/Classes/IniEmptyLine.cs
+++
@@ -1,5 +0,0 @@
-﻿using IniTools.Interfaces;
-
-namespace IniTools.Classes;
-
-public sealed class IniEmptyLine : IIniEmptyLine;
