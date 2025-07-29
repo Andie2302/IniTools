b65b1bb17a827fd942edb5230717957a52ce0d24
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 23:41:59 +0200

    Refactor `IniComment` for concise syntax and nullable improvements.

--- IniTools/Base/Classes/IniComment.cs
+++ IniTools/Base/Classes/IniComment.cs
@@ -3,51 +3,13 @@ using IniTools.Base.Interfaces;
 
 namespace IniTools.Base.Classes;
 
-public sealed class IniComment(string? comment) : IIniComment
+public sealed class IniComment ( string comment ) : IIniComment
 {
-    public string? Comment { get; set; } = comment;
-
-    public int CompareTo(IIniComment? other) 
-    {
-        if (other is null) {
-            return 1;
-        }
-
-        return string.Compare(Comment, other.Comment, StringComparison.OrdinalIgnoreCase);
-    }
-
-    public bool Equals(IIniComment? other)
-    {
-        if (other is null) {
-            return false;
-        }
-
-        if (ReferenceEquals(this, other)) {
-            return true;
-        }
-
-        return string.Equals(Comment, other.Comment, StringComparison.OrdinalIgnoreCase);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return Equals(obj as IIniComment);
-    }
-
-    public override int GetHashCode()
-    {
-        // ToLowerInvariant() für .NET Standard 2.0 Kompatibilität
-        return (Comment?.ToLowerInvariant() ?? "").GetHashCode();
-    }
-    
-    public static bool operator ==(IniComment? left, IniComment? right)
-    {
-        if (left is null) {
-            return right is null;
-        }
-
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(IniComment? left, IniComment? right) => !(left == right);
+    public string Comment { get; set; } = comment;
+    public int CompareTo ( IIniComment? other ) { return other is null ? 1 : string.Compare ( Comment , other.Comment , StringComparison.OrdinalIgnoreCase ); }
+    public bool Equals ( IIniComment? other ) { return other is not null && ( ReferenceEquals ( this , other ) || string.Equals ( Comment , other.Comment , StringComparison.OrdinalIgnoreCase ) ); }
+    public override bool Equals ( object? obj ) { return Equals ( obj as IIniComment ); }
+    public override int GetHashCode() { return ( Comment.ToLowerInvariant() ?? "" ).GetHashCode(); }
+    public static bool operator == ( IniComment? left , IniComment? right ) { return left is null ? right is null : left.Equals ( right ); }
+    public static bool operator != ( IniComment? left , IniComment? right ) => !( left == right );
 }
