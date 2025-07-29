cd98652484f0f50c15d6fcf3aec4cf3f0c0400cb
Andreas <Andreas.Zwander@gmail.com>
2025-07-29 22:30:55 +0200

    Update `IIniSection` interface to use `IIniSectionAddAble` instead of `IIniListAble`.

--- IniTools/Base/Interfaces/IIniSection.cs
+++ IniTools/Base/Interfaces/IIniSection.cs
@@ -5,5 +5,5 @@ namespace IniTools.Base.Interfaces;
 public interface IIniSection : IIniSortAble<IIniSection>, IIniElement
 {
     IIniSectionName? Name { get; }
-    List< IIniListAble > Elements { get; }
+    List< IIniSectionAddAble > Elements { get; }
 }
