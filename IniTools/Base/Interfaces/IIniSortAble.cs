8219d52fc9068047627b1b54f16b4a546cb61a83
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 01:26:15 +0200

    Remove verbose comments and redundant newlines from `IIniSortAble` interface to improve readability and maintain consistency.

--- IniTools/Base/Interfaces/IIniSortAble.cs
+++ IniTools/Base/Interfaces/IIniSortAble.cs
@@ -2,13 +2,5 @@
 
 namespace IniTools.Base.Interfaces;
 
-/// <summary>
-/// Represents a sortable interface for INI-related elements.
-/// </summary>
-/// <typeparam name="T">The type to be compared or equated.</typeparam>
-/// <remarks>
-/// This interface provides sorting functionality for INI elements. It combines comparability and equatability,
-/// enabling objects implementing this interface to define custom sorting logic and equality checks.
-/// Typical use cases include ordering and organizing elements within an INI structure based on custom or predefined criteria.
-/// </remarks>
-public interface IIniSortAble < T > : IEquatable< T > , IComparable< T > { }
+public interface IIniSortAble < T > : IEquatable< T > , IComparable< T >
+{ }
