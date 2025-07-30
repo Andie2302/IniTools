54b98f12a9290e35790de5fd2eb003e847d4575f
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 01:26:22 +0200

    Remove verbose comments from `IIniSectionAddAble` interface to improve readability and maintain consistency.

--- IniTools/Base/Interfaces/IIniSectionAddAble.cs
+++ IniTools/Base/Interfaces/IIniSectionAddAble.cs
@@ -1,12 +1,3 @@
 ﻿namespace IniTools.Base.Interfaces;
 
-/// <summary>
-/// Represents an interface for elements that can be added to an INI section.
-/// </summary>
-/// <remarks>
-/// This interface is implemented by various INI file components, such as comments,
-/// empty lines, key-value pairs, and unknown lines, to allow their inclusion
-/// within an INI section. It provides a common base for all these elements,
-/// enabling uniform handling within the INI structure.
-/// </remarks>
 public interface IIniSectionAddAble;
