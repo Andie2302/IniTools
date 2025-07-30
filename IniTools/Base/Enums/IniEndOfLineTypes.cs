fa45247895f010aa09359e3b5befa039bcb46efe
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 01:23:55 +0200

    Remove verbose comments from `IniEndOfLineTypes` enum to improve readability.

--- IniTools/Base/Enums/IniEndOfLineTypes.cs
+++ IniTools/Base/Enums/IniEndOfLineTypes.cs
@@ -1,25 +1,9 @@
 ﻿namespace IniTools.Base.Enums;
 
-/// <summary>
-/// Represents the Unix-style end-of-line type, which uses '\n' (LF) as the line terminator.
-/// Commonly used in Unix-based and Linux-based operating systems.
-/// </summary>
 public enum IniEndOfLineTypes
 {
-    /// <summary>
-    /// Represents an end-of
-    None,
-    /// <summary>
-    /// Represents the Windows-style end-of-line sequence, typically consisting of a carriage return
-    /// followed by a line feed (CR+LF).
-    /// </summary>
-    Windows,
-    /// <summary>
-    /// Represents
-    Unix,
-    /// <summary>
-    /// Represents the Mac (Classic Mac OS) style end-of-line type,
-    /// which uses the Carriage Return (CR) character ('\r') as its line terminator.
-    /// </summary>
+    None ,
+    Windows ,
+    Unix ,
     Mac
 }
