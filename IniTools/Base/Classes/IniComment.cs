6aca498a50aef281f36e0c8118df8cacab012283
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 01:23:40 +0200

    Remove verbose comments from `IniComment` class to enhance readability and simplify `GetHashCode` implementation.

--- IniTools/Base/Classes/IniComment.cs
+++ IniTools/Base/Classes/IniComment.cs
@@ -3,88 +3,13 @@ using IniTools.Base.Interfaces;
 
 namespace IniTools.Base.Classes;
 
-
-
-
-
-
-
-
 public sealed class IniComment ( string comment ) : IIniComment
 {
-    
-    
-    
-    
-    
-    
-    
     public string Comment { get; set; } = comment;
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     public int CompareTo ( IIniComment? other ) { return other is null ? 1 : string.Compare ( Comment , other.Comment , StringComparison.OrdinalIgnoreCase ); }
-
-    
-    
-    
-    
-    
-    
-    
     public bool Equals ( IIniComment? other ) { return other is not null && ( ReferenceEquals ( this , other ) || string.Equals ( Comment , other.Comment , StringComparison.OrdinalIgnoreCase ) ); }
-
-    
-    
-    
-    
-    
-    
-    
     public override bool Equals ( object? obj ) { return Equals ( obj as IIniComment ); }
-
-    
-    
-    
-    
-    
-    
-    
-    public override int GetHashCode()
-    {
-        
-        return StringComparer.OrdinalIgnoreCase.GetHashCode(Comment ?? "");
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public override int GetHashCode() { return StringComparer.OrdinalIgnoreCase.GetHashCode ( Comment ?? "" ); }
     public static bool operator == ( IniComment? left , IniComment? right ) { return left is null ? right is null : left.Equals ( right ); }
-
-    
-    
-    
-    
-    
-    
-    
-    
     public static bool operator != ( IniComment? left , IniComment? right ) => !( left == right );
 }
