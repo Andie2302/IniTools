f1d982690abbb91004490add6d92b0dbea8e14f8
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 01:26:50 +0200

    Remove verbose comments from `IIniEmptyLine` interface to improve readability and maintain consistency.

--- IniTools/Base/Interfaces/IIniEmptyLine.cs
+++ IniTools/Base/Interfaces/IIniEmptyLine.cs
@@ -1,12 +1,3 @@
 ﻿namespace IniTools.Base.Interfaces;
 
-/// <summary>
-/// Represents an empty line within an INI configuration.
-/// This interface is used to define and manage the behavior of empty lines
-/// that appear in INI files, typically for formatting or organizational purposes.
-/// </summary>
-/// <remarks>
-/// IIniEmptyLine extends the functionality from IIniLine and IIniSectionAddAble
-/// to provide behavior specific to empty lines in the context of INI structures.
-/// </remarks>
-public interface IIniEmptyLine : IIniLine<IIniEmptyLine>, IIniSectionAddAble;
+public interface IIniEmptyLine : IIniLine< IIniEmptyLine > , IIniSectionAddAble;
