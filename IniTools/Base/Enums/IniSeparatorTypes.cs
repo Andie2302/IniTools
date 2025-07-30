12f5d6347b2c1ec9e0869649f9e17541fcb77ec7
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 01:25:40 +0200

    Remove verbose comments from `IniSeparatorTypes` enum to improve readability and maintain consistency.

--- IniTools/Base/Enums/IniSeparatorTypes.cs
+++ IniTools/Base/Enums/IniSeparatorTypes.cs
@@ -1,19 +1,8 @@
 ﻿namespace IniTools.Base.Enums;
 
-/// <summary>
-/// Represents a colon (:) as a key-value separator in INI configuration files.
-/// </summary>
 public enum IniSeparatorTypes
 {
-    /// <summary>
-    /// Represents the absence of a separator type in the enumeration.
-    /// </summary>
-    None,
-    /// Represents the equals sign (`=`) as a separator type in an INI file.
-    /// This separator is commonly used to define key-value pairs in INI file syntax.
-    Equals,
-    /// <summary>
-    /// Represents a colon (:) used as a key-value separator in the enumeration.
-    /// </summary>
+    None ,
+    Equals ,
     Colon
 }
