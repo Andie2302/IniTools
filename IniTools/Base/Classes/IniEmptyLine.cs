fe1d1383497e345ab64407281f71cc2982c2d381
Andreas <Andreas.Zwander@gmail.com>
2025-07-31 04:16:59 +0200

    Refactor `GetHashCode` implementation in `IniEmptyLine` to use expression-bodied member.

--- IniTools/Base/Classes/IniEmptyLine.cs
+++ IniTools/Base/Classes/IniEmptyLine.cs
@@ -7,7 +7,7 @@ public sealed class IniEmptyLine : IIniEmptyLine
     public int CompareTo ( IIniEmptyLine? other ) { return other is null ? 1 : 0; }
     public bool Equals ( IIniEmptyLine? other ) { return other is not null; }
     public override bool Equals ( object? obj ) { return Equals ( obj as IIniEmptyLine ); }
-    public override int GetHashCode() { return typeof ( IniEmptyLine ).GetHashCode(); }
+    public override int GetHashCode() => typeof(IniEmptyLine).GetHashCode();
     public static bool operator == ( IniEmptyLine? left , IniEmptyLine? right ) { return left is null && right is null || left is not null && left.Equals ( right ); }
     public static bool operator != ( IniEmptyLine? left , IniEmptyLine? right ) => !( left == right );
 }
