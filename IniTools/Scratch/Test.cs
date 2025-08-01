a75b8690660245fbf3a138c295118a852bca03ee
Andreas <Andreas.Zwander@gmail.com>
2025-08-01 21:09:04 +0200

    a

---
+++ IniTools/Scratch/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using IniTools.Base.Interfaces;
+
+namespace IniTools.Scratch;
+
+public class IniTest
+{
+    public static void Main()
+    {
+        var desktopPath = Environment.GetFolderPath ( Environment.SpecialFolder.Desktop );
+        var pfad = Path.Combine ( desktopPath , "test.ini" );
+        var x = IniFileParser.Parse ( pfad );
+        Console.WriteLine ( "--- INI-File Content ---" );
+
+        foreach ( var section in x ) {
+            Console.WriteLine ( $"[{section.Name}]" );
+
+            foreach ( var element in section.Elements ) {
+                switch ( element ) {
+                    case IIniKeyValue kv : Console.WriteLine ( $"{kv.Key} = {kv.Value}" ); break;
+
+                    case IIniComment comment : Console.WriteLine ( $";{comment.Comment}" ); break;
+
+                    case IIniEmptyLine : Console.WriteLine(); break;
+
+                    case IIniUnknownLine unknown : Console.WriteLine ( $"?? {unknown.Line}" ); break;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
