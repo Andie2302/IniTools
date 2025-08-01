b4646a87e0da78b85bada58e6b8dcc21b19a854c
Andreas <Andreas.Zwander@gmail.com>
2025-08-01 20:38:24 +0200

    a

---
+++ IniTools/Scratch/IniFileParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using IniTools.Base.Classes;
+using IniTools.Collections.Classes;
+
+namespace IniTools.Scratch;
+
+public static class IniFileParser
+{
+    public static IniSectionCollection Parse ( string filePath )
+    {
+        var collection = new IniSectionCollection();
+        IniSection? currentSection = null;
+
+        foreach ( var line in File.ReadLines ( filePath ) ) {
+            var trimmedLine = line.Trim();
+
+            if ( trimmedLine.StartsWith ( "[" , StringComparison.InvariantCultureIgnoreCase ) && trimmedLine.EndsWith ( "]" , StringComparison.InvariantCultureIgnoreCase ) ) {
+                var sectionName = trimmedLine.Substring ( 1 , trimmedLine.Length - 2 ).Trim();
+                currentSection = new IniSection ( sectionName );
+                collection.Add ( currentSection );
+
+                continue;
+            }
+
+            if ( currentSection is null ) { continue; }
+
+            if ( string.IsNullOrWhiteSpace ( trimmedLine ) ) { currentSection.AddElement ( new IniEmptyLine() ); }
+            else {
+                if ( trimmedLine.StartsWith ( ";" , StringComparison.InvariantCultureIgnoreCase ) ) { currentSection.AddElement ( new IniComment ( trimmedLine.Substring ( 1 ) ) ); }
+                else {
+                    if ( trimmedLine.Contains ( '=' ) ) {
+                        var parts = trimmedLine.Split ( [ '=' ] , 2 );
+                        currentSection.AddElement ( new IniKeyValue ( parts[0].Trim() , parts[1].Trim() ) );
+                    }
+                    else { currentSection.AddElement ( new IniUnknownLine ( line ) ); }
+                }
+            }
+        }
+
+        return collection;
+    }
+}
