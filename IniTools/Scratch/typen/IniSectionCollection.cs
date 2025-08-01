d94b7ac0ac1b86cfdee846898438d81db5a6d40b
Andreas <Andreas.Zwander@gmail.com>
2025-08-01 14:22:25 +0200

    a

---
+++ IniTools/Scratch/typen/IniSectionCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using IniTools.Base.Interfaces;
+
+namespace IniTools.Scratch.typen;
+
+public class IniSectionCollection() : KeyedCollection< string , IIniSection > ( StringComparer.OrdinalIgnoreCase ) , IIniSectionCollection
+{
+    protected override string GetKeyForItem ( IIniSection item ) { return item.Name; }
+}
---
+++ IniTools/Scratch/typen/IniSectionLookup.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace IniTools.Scratch.typen;
+
+public class IniSectionLookup() : KeyedCollection< string , IIniSectionList > ( StringComparer.OrdinalIgnoreCase )
+{
+    protected override string GetKeyForItem ( IIniSectionList item ) { return item.SectionName; }
+}
