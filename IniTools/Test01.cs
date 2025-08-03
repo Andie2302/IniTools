95559edbe235cb51498c4529dfa6112d487f4be3
Andreas <Andreas.Zwander@gmail.com>
2025-08-04 00:44:10 +0200

    Create First proof of concept...

---
+++ IniTools/Test01.cs
@@ -0,0 +1,24 @@
+﻿using IniTools.Base;
+
+namespace IniTools;
+
+public class Test01
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        var file = Path.Combine(Environment.GetFolderPath ( Environment.SpecialFolder.Desktop ), "test.ini");
+
+        if ( File.Exists ( file ) ) {
+            Console.WriteLine ( "File exists" );
+            var x =IniParser.Parse ( file );
+
+            foreach ( var line in x ) {
+                Console.WriteLine ( line.Content?.ToString() );
+            }
+        }
+        else {
+            Console.WriteLine ( "File does not exist" );
+        }
+    }
+}
