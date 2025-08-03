96854186ce9ece2d82124a1096ea9c0c30b71c90
Andreas <Andreas.Zwander@gmail.com>
2025-08-04 01:37:21 +0200

    Create IniWriter

---
+++ IniTools/Base/IniWriter.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using IniTools.Base.ContentTypes;
+
+namespace IniTools.Base;
+
+public class IniWriter
+{
+    public void Write ( string filePath , List< IniLine > lines )
+    {
+        var content = WriteToString ( lines );
+        File.WriteAllText ( filePath , content );
+    }
+
+    public string WriteToString ( List< IniLine > lines )
+    {
+        var builder = new StringBuilder();
+
+        foreach ( var line in lines ) {
+            var contentPart = line.Content switch
+            {
+                IniSectionContent section => $"[{section.SectionName}]" ,
+                IniKeyValueContent kvp => $"{kvp.Key} = {kvp.Value}" ,
+                IniUnknownContent unknown => unknown.Text ,
+            };
+
+            if ( line.Comment != null ) {
+                if ( !string.IsNullOrEmpty ( contentPart ) ) { builder.AppendLine ( $"{contentPart} {line.Comment}" ); }
+
+                {
+                    builder.AppendLine ( line.Comment );
+                }
+            }
+
+            {
+                builder.AppendLine ( contentPart );
+            }
+        }
+
+        return builder.ToString();
+    }
+}
