f221b799be98a72f1baa72bcef53b59075843741
Andreas <Andreas.Zwander@gmail.com>
2025-08-04 01:31:47 +0200

    Create file IniSectionContent

---
+++ IniTools/Base/ContentTypes/IniSectionContent.cs
@@ -0,0 +1,7 @@
+﻿namespace IniTools.Base.ContentTypes;
+
+/// <summary>
+/// Represents a section declaration in an INI file.
+/// </summary>
+/// <param name="SectionName">The name of the section.</param>
+public sealed record IniSectionContent(string SectionName) : IniLineContent;
