8656b79604fd6dab37bcaeaaa141d5c52e2df9f6
Andreas <Andreas.Zwander@gmail.com>
2025-08-04 00:43:01 +0200

    Create IniLine

---
+++ IniTools/Base/IniLine.cs
@@ -0,0 +1,9 @@
+﻿using IniTools.Base.ContentTypes;
+
+namespace IniTools.Base;
+
+public sealed class IniLine ( IniLineContent? content , string? comment )
+{
+    public IniLineContent? Content { get; } = content;
+    public string? Comment { get; } = comment;
+}
