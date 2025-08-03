03f51f48ebd4b4cf2ba5d480fc2bc4f02c1395eb
Andreas <Andreas.Zwander@gmail.com>
2025-08-04 01:31:59 +0200

    Create file IniUnknownContent

---
+++ IniTools/Base/ContentTypes/IniUnknownContent.cs
@@ -0,0 +1,7 @@
+﻿namespace IniTools.Base.ContentTypes;
+
+/// <summary>
+/// Represents a line that could not be parsed as a known INI element, such as a key-value pair or a section.
+/// </summary>
+/// <param name="Text">The raw text of the unknown line.</param>
+public sealed record IniUnknownContent(string Text) : IniLineContent;
