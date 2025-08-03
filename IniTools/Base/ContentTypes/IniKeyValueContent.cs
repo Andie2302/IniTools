5b89f0cdbbbd700423e8e878fb1101e00632a88e
Andreas <Andreas.Zwander@gmail.com>
2025-08-04 01:30:31 +0200

    Create IniKeyValueContent

---
+++ IniTools/Base/ContentTypes/IniKeyValueContent.cs
@@ -0,0 +1,8 @@
+﻿namespace IniTools.Base.ContentTypes;
+
+/// <summary>
+/// Represents a key-value pair from an INI file.
+/// </summary>
+/// <param name="Key">The key of the setting.</param>
+/// <param name="Value">The value of the setting.</param>
+public sealed record IniKeyValueContent(string Key, string Value) : IniLineContent;
